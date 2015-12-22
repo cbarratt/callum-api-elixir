d569ce7e783f4c72b2a92bf03ff981ff994ef9db
Callum Barratt <callum.barratt@sky.uk>
2015-12-22 15:19:26 +0000

    Refactor bitcoin controllah

--- web/controllers/bitcoin_controller.ex
+++ web/controllers/bitcoin_controller.ex
@@ -2,12 +2,18 @@ defmodule Callumapi.BitcoinController do
   use Callumapi.Web, :controller
 
   def index(conn, _params) do
+    data = Task.async(fn -> grab_balance end)
+
+    render(conn, balance: Task.await(data))
+  end
+
+  def grab_balance do
     password = System.get_env("BLOCK_PASS")
 
-    data = HTTPoison.get!("https://blockchain.info/merchant/2f4e54fe-e4ad-b0de-205a-93492b9ccf7d/address_balance?password=#{password}&address=1M5ahj4ZX6YqYC7BBpni8Kwio949J1invd").body |> Poison.decode!
-
-    json = "0.#{data["balance"]}"
-
-    render(conn, balance: json)
+    HTTPoison.get!("https://blockchain.info/merchant/2f4e54fe-e4ad-b0de-205a-93492b9ccf7d/address_balance?password=#{password}&address=1M5ahj4ZX6YqYC7BBpni8Kwio949J1invd").body
+    |> Poison.decode!
+    |> convert
   end
+
+  defp convert(data), do: "0.#{data["balance"]}"
 end
