2ba4f871a65d3f5d6e4c36d01f661b93929e2e97
Callum Barratt <callum.barratt@sky.uk>
2016-01-26 10:56:02 +0000

    Remove docker stuff and amend circle config

--- circle_pre_build.sh
+++ circle_pre_build.sh
@@ -13,15 +13,15 @@ asdf plugin-add erlang https://github.com/HashNuke/asdf-erlang.git
 asdf plugin-add elixir https://github.com/HashNuke/asdf-elixir.git
 
 # Extract vars from elixir_buildpack.config
-. elixir_buildpack.config
+#. elixir_buildpack.config
 
 # Write .tool-versions
-echo “erlang $erlang_version” >> .tool-versions
-echo “elixir $elixir_version” >> .tool-versions
+#echo “erlang $erlang_version” >> .tool-versions
+#echo “elixir $elixir_version” >> .tool-versions
 
 # Install erlang/elixir
-asdf install erlang $erlang_version
-asdf install elixir $elixir_version
+asdf install erlang 18.2.1
+asdf install elixir 1.2.1
 
 # Get dependencies
 yes | mix deps.get
--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$1" = 'server' ]; then
-  MIX_ENV=prod PORT=4000 mix compile && mix phoenix.server
-fi
-
-exec "$@"
