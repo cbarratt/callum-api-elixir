81532ed09372e090c1dbe26621bad6841a59c5f6
Callum Barratt <callum.barratt@sky.uk>
2016-02-04 11:34:46 +0000

    Added SASS to brunch

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,11 +1,1 @@
-import {Socket} from "phoenix"
 import "phoenix_html"
-
-// let socket = new Socket("/ws")
-// socket.join("topic:subtopic", {}, chan => {
-// })
-
-let App = {
-}
-
-export default App
